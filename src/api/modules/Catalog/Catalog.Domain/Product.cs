3639b8fef4c4e095d1d2c3e8932d3ca105c0218b
Vipul Malhotra <vipulm124@gmail.com>
2024-07-24 17:28:50 +0530

    added imagepath field to product class

--- src/api/modules/Catalog/Catalog.Domain/Product.cs
+++ src/api/modules/Catalog/Catalog.Domain/Product.cs
@@ -8,38 +8,42 @@ public class Product : AuditableEntity, IAggregateRoot
     public string Name { get; private set; } = default!;
     public string? Description { get; private set; }
     public decimal Price { get; private set; }
+    public string? ImagePath { get; private set; }
 
-    public static Product Create(string name, string? description, decimal price)
+    public static Product Create(string name, string? description, decimal price, string? imagePath = null)
     {
         var product = new Product();
 
         product.Name = name;
         product.Description = description;
         product.Price = price;
+        product.ImagePath = imagePath;
 
         product.QueueDomainEvent(new ProductCreated() { Product = product });
 
         return product;
     }
 
-    public Product Update(string? name, string? description, decimal? price)
+    public Product Update(string? name, string? description, decimal? price, string? imagePath = null)
     {
         if (name is not null && Name?.Equals(name, StringComparison.OrdinalIgnoreCase) is not true) Name = name;
         if (description is not null && Description?.Equals(description, StringComparison.OrdinalIgnoreCase) is not true) Description = description;
         if (price.HasValue && Price != price) Price = price.Value;
+        if (imagePath is not null && ImagePath?.Equals(imagePath, StringComparison.OrdinalIgnoreCase) is not true) ImagePath = imagePath;
 
         this.QueueDomainEvent(new ProductUpdated() { Product = this });
         return this;
     }
 
-    public static Product Update(Guid id, string name, string? description, decimal price)
+    public static Product Update(Guid id, string name, string? description, decimal price, string? imagePath = null)
     {
         var product = new Product
         {
             Id = id,
             Name = name,
             Description = description,
-            Price = price
+            Price = price,
+            ImagePath = imagePath
         };
 
         product.QueueDomainEvent(new ProductUpdated() { Product = product });
