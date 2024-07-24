f906f79c14758adb327fcff725cc35b3d04370b3
Vipul Malhotra <vipulm124@gmail.com>
2024-07-24 17:48:40 +0530

    added migration

--- src/api/modules/Catalog/Catalog.Application/Products/Create/v1/CreateProductCommand.cs
+++ src/api/modules/Catalog/Catalog.Application/Products/Create/v1/CreateProductCommand.cs
@@ -6,4 +6,5 @@ namespace FSH.Starter.WebApi.Catalog.Application.Products.Create.v1;
 public sealed record CreateProductCommand(
     [property: DefaultValue("Sample Product")] string? Name,
     [property: DefaultValue(10)] decimal Price,
-    [property: DefaultValue("Descriptive Description")] string? Description = null) : IRequest<CreateProductResponse>;
+    [property: DefaultValue("Descriptive Description")] string? Description = null,
+    string? imagePath = null) : IRequest<CreateProductResponse>;
