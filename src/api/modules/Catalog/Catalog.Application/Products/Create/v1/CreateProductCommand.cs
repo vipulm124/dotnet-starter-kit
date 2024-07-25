a53d56eed069aba4d9ef0c8ec13685ee847a2e1d
Vipul Malhotra <vipulm124@gmail.com>
2024-07-25 11:33:51 +0530

    something

--- src/api/modules/Catalog/Catalog.Application/Products/Create/v1/CreateProductCommand.cs
+++ src/api/modules/Catalog/Catalog.Application/Products/Create/v1/CreateProductCommand.cs
@@ -7,4 +7,4 @@ public sealed record CreateProductCommand(
     [property: DefaultValue("Sample Product")] string? Name,
     [property: DefaultValue(10)] decimal Price,
     [property: DefaultValue("Descriptive Description")] string? Description = null,
-    string? imagePath = null) : IRequest<CreateProductResponse>;
+    byte[]? ImagePath = null) : IRequest<CreateProductResponse>;
